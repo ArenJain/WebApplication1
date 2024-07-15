1d37451cb43c3c5506bdcd7f25e53bbf4651f9c0
ArenJain <arihant.jain@sourcecodecontrol.com>
2024-07-15 02:22:00 -0700

    added model for Organisation

---
+++ Models/Organisation.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Models
+{
+    public class Organisation
+    {
+ 
+        public Guid OrganisationId { get; set; }
+        public string OrganisationName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactEmail { get; set; }
+        public string Owner { get; set; }
+        public string ReportUrl { get; set; }
+        public int? ReportProgress { get; set; }
+        public string ReportCurrencyCode { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public bool IsArchived { get; set; }
+    }
+}
